4887af6040ec6bc3932147c36a89cbbdcdd75959
Jordan Ferdinand <253198146+jordan-ferdinand_IMSI@users.noreply.github.com>
2026-03-06 15:36:29 +0700

    add: soal 2

--- answer.js
+++ answer.js
@@ -35,3 +35,48 @@ const user = new UserProfile("Ada", "Lovelace", "ada@example.com");
 user.getFullName(); // "Ada Lovelace"
 user.updateEmail("bademail"); // throws Error("Invalid email format")
 user.getDaysSinceRegistration(); // e.g., 0 (if run same day)
+
+// exercise 2
+class ShoppingCart {
+  items = new Map();
+  existingItem;
+  addItem({ id, name, price, quantity }) {
+    this.existingItem = this.items.get(id);
+    if (price < 0) return Error("price should never be negative");
+    if (!this.items.has(id)) {
+      this.items.set(id, { id, name, price, quantity });
+    } else {
+      this.items.set(id, {
+        id,
+        name,
+        price,
+        quantity: this.existingItem.quantity + quantity,
+      });
+    }
+  }
+  removeItem(id) {
+    this.items.delete(id);
+  }
+  getTotal() {
+    let total = 0;
+    for (const item of this.items.values()) {
+      total = item.price * item.quantity;
+    }
+    return total;
+  }
+  getItemCount() {
+    let count = 0;
+    for (const item of this.items.values()) {
+      count += item.quantity;
+    }
+    return count;
+  }
+}
+
+const cart = new ShoppingCart();
+cart.addItem({ id: 1, name: "Keyboard", price: 80, quantity: 1 });
+cart.addItem({ id: 1, name: "Keyboard", price: 80, quantity: 2 });
+console.log(cart.getItemCount()); // 3
+console.log(cart.getTotal()); // 240
+cart.removeItem(1);
+console.log(cart.getTotal()); // 0
